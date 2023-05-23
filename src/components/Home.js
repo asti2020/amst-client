a2412c5573e04f3e08cfea06117e2fc8c7da3228
Astika Luintel <astild2020@gmail.com>
2023-05-23 15:31:08 -0400

    initials

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+function Home(){
+    return(
+        <>
+        <div>
+            <NavBar />
+        </div>
+        <div>
+            <h1>Welcome to the Home page!</h1>
+        </div>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/Profile.js
@@ -0,0 +1,9 @@
+function Profile(){
+    return (
+        <div>
+            <h1>Create yours!</h1>
+        </div>
+    )
+}
+
+export default Profile;
