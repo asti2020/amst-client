861b8d73927cdbd4d35d6e1c3bc0b6a628f5256e
Astika Luintel <astild2020@gmail.com>
2023-05-24 17:45:54 -0400

    init

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,19 @@
-import { NavLink } from "react-router-dom";
+
 
 function NavBar (){
     return(
         <>
-         {/* <div className="navbar-center">
-                <NavLink exact to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/Profile">Portfolio</NavLink>
-            </div>  */}
+            <div>
+              <div></div>
 
-<div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered" />
-    </div>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0jK8YysGKIzo9RBLbbtQiGfvpjgNTE7W_Gg&usqp=CAU" />
-        </div>
-      </label>
-      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+              <div className="dropdown">
+                  <span>Mouse over me</span>
+                  <div className="dropdown-content">
+                    <p>Hello World!</p>
+                  </div>
+              </div>
+              
+            </div>
             
         </>
     )
