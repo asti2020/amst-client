691b8ff355ad64c9c5fb2cadd1bac27f8332a82f
Astika Luintel <astild2020@gmail.com>
2023-06-01 15:34:02 -0400

    banner

---
+++ src/components/Banner.js
@@ -0,0 +1,19 @@
+function Banner(){
+    return (
+        <>
+            <div className="flex justify-center p-5 m-2 align-center">
+                <div className="">
+                    <img src="https://www.bannerflow.com/app/uploads/blog-header-creative-banner.jpg"
+                     alt=""
+                      className="banner-image "/>
+                </div>
+                <div className="text-end">
+                    <h2>Astika Luintel Kriti</h2>
+                    <p>I am a pro software engineer</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Banner
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './NavBar';
 import PostCard from './PostCard';
+import Banner from './Banner'
 
 function Home(){
     return(
@@ -8,6 +9,11 @@ function Home(){
         <div>
             <NavBar />
         </div>
+
+        <div>
+            <Banner />
+        </div>
+        
         <div>
             <PostCard />
             <h1>Welcome to the Home page!</h1>
