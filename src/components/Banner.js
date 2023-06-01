8121cf8973ee0adc4141a04bf9a3ff70b6825645
Astika Luintel <astild2020@gmail.com>
2023-06-01 16:08:28 -0400

    init

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,20 @@
 function Banner(){
     return (
         <>
-            <div className="flex justify-center p-5 m-2 align-center">
+            <div className="p-1 m-1 ">
                 <div className="">
                     <img src="https://www.bannerflow.com/app/uploads/blog-header-creative-banner.jpg"
-                     alt=""
-                      className="banner-image "/>
+                        alt=""
+                        className="banner-image"
+                        width="100%"
+                    />
+                
+                <div className="text-end relative md:absolute">
+                    <h2 className="">
+                        Astika Luintel Kriti
+                    </h2>
+                    <p>I am a Full-Stack software engineer</p>
                 </div>
-                <div className="text-end">
-                    <h2>Astika Luintel Kriti</h2>
-                    <p>I am a pro software engineer</p>
                 </div>
             </div>
         </>
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -7,7 +7,7 @@ function PostCard(){
             <div className="card object-cover box-border shadow-2xl w-50 p-8 m-8">
                 <h5 className="card-title">Title</h5>
                 <div className="card-body">
-                    <img src="https://img.freepik.com/free-vector/flat-women-s-history-month-illustration_23-2149276863.jpg"
+                    <img src="https://www.nichepursuits.com/wp-content/uploads/2023/03/banner-des-1200x900.png"
                     className="object-fit-cover"
                     height="400px"
                     width="400px"
